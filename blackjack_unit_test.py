dd7f2fc2b8c398e5869d6cf12dbf0caf7ed58f3d
Mike Caprio <mike.caprio@neo.com>
2013-12-02 16:04:41 -0500

    Modify blackjack testing, add .gitignore

--- blackjack_unit_test.py
+++ blackjack_unit_test.py
@@ -2,17 +2,25 @@ import random
 
 from game_library.card import *
 
+def print_deck(this_deck):
+    int_counter = 0
+
+    for each_card in this_deck:
+        int_counter += 1
+
+        print str(int_counter) + ":" + each_card.get_card()
+
 
 # First create the deck
 deck = create_deck()
 
+# Show us the deck
+print_deck(deck)
+
+print "\n"
+
 # Shuffle the deck
 random.shuffle(deck)
 
-int_counter = 0
-
-for each_card in deck:
-    int_counter += 1
-
-    print str(int_counter) + ":" + each_card.get_card()
-
+# Show us the shuffled deck
+print_deck(deck)
